26d91391a59095c4370e7f732e016170cdc540c5
Maxim Aleynikov <max.aleyinikov@gmail.com>
2022-05-06 18:45:53 +0300

    first commit

---
+++ src/routes/invoices.tsx
@@ -0,0 +1,60 @@
+import {
+  NavLink,
+  Outlet,
+  useSearchParams
+} from 'react-router-dom';
+import {
+    getInvoices,
+    Invoice
+} from '../data';
+
+export const Invoices = () => {
+  const invoices = getInvoices();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem"
+        }}
+      >
+        <input
+          value={searchParams.get('filter') || ''}
+          onChange={(e) => {
+            const filter = e.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice: Invoice) => {
+            const filter = searchParams.get('filter');
+            if (!filter) return true;
+            const name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice: Invoice) => (
+          <NavLink
+            style={({isActive}) => {
+              return {
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : ""
+              };
+            }}
+            key={invoice.number}
+            to={`/invoices/${invoice.number}`}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
